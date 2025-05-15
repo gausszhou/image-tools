722549aaf3119be1435429e899bb5e5c7176b6f4
gausszhou <gausszhou@qq.com>
2025-05-15 20:49:16 +0800

    style: 调整图片信息卡片的样式

--- src/components/ProcessNodeDestination.tsx
+++ src/components/ProcessNodeDestination.tsx
@@ -4,7 +4,7 @@ import { blobToBase64, copyToClipboard, formatFileSize } from '../utils';
 import './ProcessNodeDestination.css';
 
 interface DesinationProps {
-    title: string; // 图片上方标题
+    title: string;
     image: ImageInfo;
 }
 
@@ -12,7 +12,6 @@ const ProcessNodeDestination: React.FC<DesinationProps> = ({
     title,
     image
 }) => {
-
     const copyBase64 = async () => {
         const base64 = await blobToBase64(image.blob)
         copyToClipboard(base64)
@@ -21,22 +20,38 @@ const ProcessNodeDestination: React.FC<DesinationProps> = ({
     const { width, height } = image.dimensions;
     return (
         <div className="process-node-destination">
-            <h3 className="destination-title">{title}</h3>
-            <img className="destination-image" src={url} alt={title} />
+            <div className="destination-image-container">
+                <img className="destination-image" src={url} alt={title} />
+            </div>
             <div className="process-node-destination__file-info">
-                <p><span className="file-info__label">文件名称</span><span className='file-name'>{name}</span></p>
-                <p><span className="file-info__label">文件大小</span>{formatFileSize(size)}</p>
-                <p><span className="file-info__label">图像宽高</span>{`${width} × ${height}`}</p>
+                <p>{name}</p>
+                <p>{formatFileSize(size)} </p>
+                <p>{`${width} × ${height}`}</p>
             </div>
             <div className="process-node-destination__button-group">
                 <a
                     href={url}
                     download={name}
                     className="process-node-destination__link"
+                    title="下载图片"
                 >
-                    下载图片
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/>
+                        <polyline points="7 10 12 15 17 10"/>
+                        <line x1="12" y1="15" x2="12" y2="3"/>
+                    </svg>
                 </a>
-                <a href="javascript:;" onClick={copyBase64} className="process-node-destination__link">复制 Base64</a>
+                <a 
+                    href="javascript:;" 
+                    onClick={copyBase64} 
+                    className="process-node-destination__link"
+                    title="复制 Base64"
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <rect x="9" y="9" width="13" height="13" rx="2" ry="2"/>
+                        <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"/>
+                    </svg>
+                </a>
             </div>
         </div>
     );
